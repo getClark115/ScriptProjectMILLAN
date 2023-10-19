39c9e1766ceaf0636847a2888ccd730b59eb1fba
Clark Rodriguez Millan <740394@student.dusd.net>
2023-10-19 15:10:05 -0700

    make cololr
    make color

--- scriptTMILLANPER7/Assets/Scripts/ColorChanger.cs
+++ scriptTMILLANPER7/Assets/Scripts/ColorChanger.cs
@@ -7,7 +7,7 @@ public class ColorChanger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,5 +17,13 @@ public class ColorChanger : MonoBehaviour
         {
             GetComponent<Renderer>().material.color = Color.red;
         }
-    }    
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
 }
