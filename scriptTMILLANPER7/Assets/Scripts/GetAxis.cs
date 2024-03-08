e189efe47be094ccdefbc343366297b8073948fa
Clark Rodriguez Millan <740394@student.dusd.net>
2024-03-08 15:02:26 -0800

    did more scripts
    latest script i did was the get axis

--- scriptTMILLANPER7/Assets/Scripts/GetAxis.cs
+++ scriptTMILLANPER7/Assets/Scripts/GetAxis.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyComponent : MonoBehaviour
+public class GetAxis : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,9 +13,6 @@ public class DestroyComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            Destroy(GetComponent<MeshRenderer>());
-        }
+        
     }
 }
